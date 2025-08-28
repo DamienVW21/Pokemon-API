643c047d04f1c12051226ef1a6f0ee774c28756d
Damien <dvw2011@hotmial.com>
2025-08-27 20:45:10 -0400

    Set up connection for pokemon api

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import org.example.models.Pokemon;
 import org.example.services.DadJokeService;
+import org.example.services.PokemonService;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +15,14 @@ public class Main {
 
         String joke = jokeService.getDadJoke();
         System.out.println(joke);
+
+        // create pokemonservice object to query the api
+        PokemonService pokemonService = new PokemonService();
+        // it returns a list of pokemon objects
+        List<Pokemon> list = pokemonService.getAllPokemon();
+        System.out.println(list);
     }
 }
+
+
+
--- src/main/java/org/example/services/PokemonService.java
+++ src/main/java/org/example/services/PokemonService.java
@@ -1,9 +1,22 @@
 package org.example.services;
 
+import org.example.models.Pokemon;
+import org.example.models.Results;
 import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 public class PokemonService {
     private final String API_URL = "https://pokeapi.co/api/v2/pokemon";
 
     private RestClient restClient = RestClient.create();
+
+    public List<Pokemon> getAllPokemon(){
+        Results rs = restClient
+                .get()
+                .uri(API_URL)
+                .retrieve()
+                .body(Results.class);
+        return rs.getResults();
+    }
 }
