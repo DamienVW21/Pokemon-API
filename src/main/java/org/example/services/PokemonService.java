7a71f722563ed89c3f58c7209bd6f96f83397e87
Damien <dvw2011@hotmial.com>
2025-08-27 21:00:00 -0400

    Grabbed and extracted the ID from the URL

--- src/main/java/org/example/services/PokemonService.java
+++ src/main/java/org/example/services/PokemonService.java
@@ -19,4 +19,31 @@ public class PokemonService {
                 .body(Results.class);
         return rs.getResults();
     }
+    public List<Pokemon> getMorePokemon(int startVal){
+        String moreUrl = API_URL + "offset=" + startVal + "&limit=20";
+
+        Results rs = restClient
+                .get()
+                .uri(moreUrl)
+                .retrieve()
+                .body(Results.class);
+        List<Pokemon> list = rs.getResults();
+
+        // Want to loop through the list of pokemon obj.
+        // need to parse out the id number and add to each pokemon object
+
+        for (Pokemon p : list){
+            String url = p.getUrl(); // grabbing url here
+            // now need index of where pokemon/ is in the url
+            int pokemonIndex = url.indexOf("pokemon/");
+
+            // pokemon/id/
+            String pokemonUrl = url.substring(pokemonIndex);
+
+            int slashIndex = pokemonUrl.indexOf("/");
+            int pokemonId =
+                    Integer.parseInt(pokemonUrl.substring(slashIndex + 1, pokemonUrl.length() - 1));
+            p.setId(pokemonId);
+        }
+    }
 }
