0501e6dee1b84410df5886845919ba40bbd5b95d
Damien <dvw2011@hotmail.com>
2025-09-02 20:53:51 -0400

    Added base functionality test with simple menu to pull pokemon by ID

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.example.services.DadJokeService;
 import org.example.services.PokemonService;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,9 +19,43 @@ public class Main {
 
         // create pokemonservice object to query the api
         PokemonService pokemonService = new PokemonService();
-        // it returns a list of pokemon objects
-        List<Pokemon> list = pokemonService.getMorePokemon(20);
-        System.out.println(list);
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Choose from the following menu:");
+            System.out.println("1. return the first 20 pokemon");
+            System.out.println("2. return the next 20 pokemon");
+            System.out.println("3. return the next 20 pokemon");
+            System.out.println("5. quit");
+            System.out.println("Enter choice: ");
+            int choice = Integer.parseInt(input.nextLine());
+            int startVal = 0;
+
+        switch (choice){
+            case 1:
+                break;
+            case 2:
+                startVal = 20;
+                break;
+            case 3:
+                startVal = 40;
+                break;
+            case 4:
+                startVal = 60;
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("Invalid Entry - try again");
+        }
+        if (choice == 5){
+            break;
+        }
+            // it returns a list of pokemon objects
+            List<Pokemon> list = pokemonService.getMorePokemon(startVal);
+            System.out.println(list);
+        }
     }
 }
 
